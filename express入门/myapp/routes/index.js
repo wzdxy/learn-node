6768be9ba9f13adf56af7d39508d40a214f56c69
zc <zc_93@qq.com>
2016-10-13 09:02:51 +0800

    login

--- express入门/myapp/routes/index.js
+++ express入门/myapp/routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 // var user=require('d:/data/db').user;
+// var MongoClient = require('mongodb').MongoClient;
 
 var Db = require('mongodb').Db,
     Server = require('mongodb').Server;
@@ -17,18 +18,30 @@ router.get('/login',function(req,res){
 })
 
 router.post('/ucenter',function(req,res){
-  var query={name:req.body.name,pw:req.body.password};
-  (function(){
-    user.count(query,function(err,doc){
-      if(doc==1){
-        console.log(query.name+':登录');
-        res.render('ucenter',{title:'ucenter'});
-      }else{
-        console.log(query.name+':失败');
-        res.redirect('/');
-      }
-    })
-  })(query);
+  var query='{"name":"'+req.body.name+'","pw":"'+req.body.password+'"}';
+  // var query={name:req.body.name,pw:req.body.password};
+  // console.log(user.find(query));
+  
+  user.find(query).toArray(function(err,result){
+    if(err){
+      console.log('Error'+err);
+      return;
+    }
+    console.log(result);
+  })
+
+  
+  // (function(){
+  //   user.count(query,function(err,doc){
+  //     if(doc==1){
+  //       console.log(query.name+':登录');
+  //       res.render('ucenter',{title:'ucenter'});
+  //     }else{
+  //       console.log(query.name+':失败');
+  //       res.redirect('/');
+  //     }
+  //   })
+  // })(query);
 })
 
 module.exports = router;
